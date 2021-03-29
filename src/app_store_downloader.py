165b8f7ec4790edf5c4fc6966f4a3a9699a7966a
meghanabhange <meghanabhange13@gmail.com>
2021-03-29 13:04:12 +0530

    Update src/app_store_downloader.py
    Co-authored-by: Nirant <NirantK@users.noreply.github.com>

--- src/app_store_downloader.py
+++ src/app_store_downloader.py
@@ -15,9 +15,8 @@ def download(app_name: str, country: str = "us", count: int = 200):
         app = AppStore(country=country, app_name=app_name)
         app.review(how_many=count)
         result = app.reviews
-        with open(
-            str(data_path / f"{app_name}_{country}_app_store_reviews.json"), "w"
-        ) as f:
+file_path = data_path / f"{app_name}_{country}_app_store_reviews.json
+with file_path.open("w") as f:
             json.dump(result, f, indent=2, default=str)
     except Exception as e:
         print(f"Couln't fetch the reviews for {app_name} with following error : {e}")
