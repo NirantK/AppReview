e52c27ee1ad012bf75d95a2d2ccd00070991659a
Nirant <NirantK@users.noreply.github.com>
2021-05-29 16:32:59 +0530

    Add Nouns, Entities

--- src/app.py
+++ src/app.py
@@ -6,18 +6,20 @@ cd src
 streamlit run app.py
 """
 
-from logging import disable
-from numpy import load
+from collections import Counter
+
+import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 import spacy
 import streamlit as st
-from streamlit.elements.text_widgets import TextWidgetsMixin
-import seaborn as sns
-import textacy
-import matplotlib.pyplot as plt
+from textacy import Corpus, extract
+from textacy.extract.basics import entities, ngrams, noun_chunks
 
 from pathlib import Path
 from typing import List
+
+DEBUG = True
 
 # Define Text
 st.title("Text Exploration App")
@@ -31,6 +33,12 @@ clients = {"Uber": data_dir / "Uber_us_app_store_reviews.json"}
 file_path = clients["Uber"]
 assert file_path.exists()
 
+st.sidebar.title("Options")
+word_count = st.sidebar.checkbox("Word Counts")
+ngrams_count = st.sidebar.checkbox("n-grams")
+show_ncs = st.sidebar.checkbox("Noun", value=True)
+show_ents = st.sidebar.checkbox("Entities")
+
 
 @st.cache
 def load_data(file_path: Path) -> List[str]:
@@ -43,7 +51,7 @@ def load_data(file_path: Path) -> List[str]:
 
 
 def make_corpus(reviews: List[str]):
-    corpus = textacy.Corpus("en_core_web_sm", data=reviews)
+    corpus = Corpus("en_core_web_sm", data=reviews)
     return corpus
 
 
@@ -52,7 +60,9 @@ reviews = load_data(file_path)
 count = st.number_input(
     label="Number of Reviews you want to analyze", value=50, max_value=len(reviews)
 )
-reviews = reviews[:count]
+
+if DEBUG:
+    reviews = reviews[:count]
 
 corpus = make_corpus(reviews)
 st.write(
@@ -67,13 +77,62 @@ def get_word_counts(
         by="lemma_", filter_stops=True, filter_nums=True, filter_punct=True
     )
     most_common = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)[:25]
-    most_common_df = pd.DataFrame(most_common, columns=["words", "count"])
+    most_common_df = pd.DataFrame(most_common, columns=["Words", "Count"])
     return most_common_df
 
 
-most_common_df = get_word_counts(corpus)
-st.dataframe(most_common_df[:10])
+if word_count:
+    st.write("## Word Counts")
 
-fig, ax = plt.subplots()
-sns.scatterplot(data=most_common_df[:10], x="count", y="words")
-st.pyplot(fig)
+    most_common_df = get_word_counts(corpus)
+    if DEBUG:
+        st.dataframe(most_common_df[:10])
+
+    fig, ax = plt.subplots()
+    sns.scatterplot(data=most_common_df[:10], x="count", y="words")
+    st.pyplot(fig)
+
+
+def spacy_span_hash(input):
+    return str(input)
+
+
+@st.cache(hash_funcs={spacy.tokens.span.Span: spacy_span_hash})
+def get_count(list_of_doc_terms: List[List], lower=True) -> pd.DataFrame:
+    if lower:
+        terms_list = [
+            str(val).lower() for sublist in list_of_doc_terms for val in sublist
+        ]
+    else:
+        terms_list = [str(val) for sublist in list_of_doc_terms for val in sublist]
+    terms_freq = dict(Counter(terms_list))
+    df = pd.DataFrame.from_dict(terms_freq, orient="index")
+    df.reset_index(inplace=True)
+    df.rename(columns={"index": "Phrase", 0: "Count"}, inplace=True)
+    # st.write(df.columns)
+    df.sort_values(by=["Count", "Phrase"], inplace=True, ascending=False)
+    return df
+
+
+def get_linguistic_counts(corpus=corpus, by="noun_chunks", **kwargs):
+    if by == "noun_chunks":
+        list_of_doc_terms = [
+            list(noun_chunks(doc, drop_determiners=True)) for doc in corpus
+        ]
+    elif by == "ents":
+        list_of_doc_terms = [
+            list(entities(doc, drop_determiners=True, exclude_types="NUMERIC"))
+            for doc in corpus
+        ]
+    return get_count(list_of_doc_terms)
+
+
+if show_ncs:
+    st.write("## Noun Phrases")
+    st.write(get_linguistic_counts(corpus))
+
+if show_ents:
+    st.write("## Entities")
+    st.write(get_linguistic_counts(corpus, by="ents"))
+    # terms_df = pd.DataFrame(dict(Counter(terms)))
+    # st.write(dict(Counter(terms)))
